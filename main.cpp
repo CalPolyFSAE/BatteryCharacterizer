14b60f637e5631c04e90e9f2e728068c75820eed
Nick Mah <nickjmah@gmail.com>
2016-12-16 17:10:08 -0800

    Turned files into an eclipse project, added comments describing function of project

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
+/**
+ * Project: Battery Characterizer
+ * Author(s): Nick Mah and ???
+ * Project Desc.: Code to characterize batteries: MCU charges
+ * batteries to 3.6-3.8V, then does 30A CC discharge to determine
+ * ESR then charges to 4.2V, then discharges at 30W CP discharge
+ * monitoring current, voltage and time to get energy, then
+ * recharges battery to 3.6-3.7V while writing data to values on
+ * spreadsheet on computer that it's connected to.
+ * File Desc.: Main file for Battery Characterizer
+ * Date: 12/16/2016
+ */
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
+int main(){
+
+}
