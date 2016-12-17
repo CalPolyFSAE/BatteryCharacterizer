56402432649ca652546bbc00b8cb9a99bf4e80a2
Nick Mah <nickjmah@gmail.com>
2016-12-16 18:49:52 -0800

    Added in functions to layout general idea of how the code should work

--- main.cpp
+++ main.cpp
@@ -14,6 +14,46 @@
 #include <stdio.h>
 #include <string.h>
 
+uint32_t read_current(uint32_t batt){
+	uint32_t current;
+	return current;
+}
+
+uint32_t read_volatge(uint32_t batt){
+	uint32_t voltage;
+	return voltage;
+}
+
+uint32_t charge_3_65(){
+	uint32_t x;
+	return x;
+}
+
+uint32_t charge_4_2(){
+	uint32_t x;
+	return x;
+}
+
+uint32_t discharge_30A(){
+
+	uint32_t time;
+	return time;
+}
+
+uint32_t discharge_30W(){
+
+	uint32_t time;
+	return time;
+
+}
+
 int main(){
+
+	uint32_t vBatt0, iBatt0, pBatt0, vBatt1, iBatt1, pBatt1;
+	uint32_t vBatt2, iBatt2, pBatt2, vBatt3, iBatt3, pBatt3;
+	pBatt0 = vBatt0 * iBatt0; -- getting power of battery
+	pBatt1 = vBatt1 * iBatt1;
+	pBatt2 = vBatt2 * iBatt2;
+	pBatt3 = vBatt3 * iBatt3;
 
 }
