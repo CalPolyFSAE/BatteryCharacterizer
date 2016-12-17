4aeda16804fcc25a4bf43d05d11bccc9eb30aff2
Nick Mah <nickjmah@gmail.com>
2016-12-16 23:22:39 -0800

    Added preliminary pin mappings(Made a comment with all the pin/define names and the official name of pin)

--- main.cpp
+++ main.cpp
@@ -16,6 +16,28 @@
 #include <string.h>
 /** pin mappings:
  *
+ * T_Batt_1 = PC5
+ * T_Batt_2 = PC4
+ * T_Batt_3 = PC3
+ * T_Batt_4 = PC2
+ * RX[UART] = PD0
+ * TX[UART] = PD1
+ * CS0[SPI] = PB2
+ * MOSI[SPI] = PB3
+ * MISO [SPI] = PB4
+ * SCK [SPI] = PB5
+ * CHARGE_1_1 = PB1
+ * CHARGE_2_1 = PD6
+ * CHARGE_EN_1 = PB0
+ * BATT_EN_1_1 = ADC6
+ * BATT_EN_2_1 = PD7
+ * DISC_EN_1 = PD5
+ * CHARGE_1_2 = PD2
+ * CHARGE_2_2 = ADC7
+ * CHARGE_EN_2 = PD4
+ * BATT_EN_1_2 = PD3
+ * BATT_EN_2_2 = PC0
+ * DISC_EN_2 = PC1
  *
  */
 uint32_t read_current(uint32_t batt){
@@ -34,12 +56,13 @@ uint32_t charge(uint32_t batt, uint32_t vStop){
 	return x;
 }
 
-uint32_t discharge_30A(batt){
+uint32_t discharge_30A(uint32_t batt){
 
 	uint32_t time;
 	return time;
 }
-/** Might want to change the discharge functions so
+/** Needed Changes
+ * Might want to change the discharge functions so
  * we only have 1 function with a 30A CC and 30W CP
  * option on the parameters. Not sure how to
  * implement this though. Code for CC and CP are almost
@@ -53,7 +76,7 @@ uint32_t discharge_30A(batt){
  */
 uint32_t discharge_30W(uint32_t batt, uint32_t vStop){
 
-	uint32_t time, voltage, current;
+	uint32_t time, voltage, current, disc_en;
 	voltage = read_voltage(batt);
 	current = read_current(batt);
 	while(voltage >= vStop){
